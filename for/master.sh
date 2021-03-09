b1b8d8c34396ac8d789302bd99c15997f8335f27
ZyCromerZ <neetroid97@gmail.com>
2021-03-10 01:51:21 +0700

    for/master: compile my boy
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/master.sh
+++ for/master.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
-# KernelBranch="20210205/neutrino-3"
+KernelBranch="20210205/neutrino-3-custom"
 
-# IncludeFiles "${MainPath}/device/begonia-q-oss.sh"
-# CustomUploader="Y"
-# IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
-# FolderUp="keqing-drive"
-# TypeBuildTag="[NON-CFW][TEST]"
+IncludeFiles "${MainPath}/device/begonia-q-oss.sh"
+CustomUploader="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
+FolderUp="keqing-drive"
+TypeBuildTag="[NON-CFW][PRIVATE][SLMK]"
 
-# CloneKernel
-# CloneGccten
+CloneKernel
+CloneGccten
 # CompileGccKernel && CleanOut
-# CloneGugelClang
+CloneGugelClang
+CompileClangKernel && CleanOut
 # pullSlmk && PullLtoSlmk
 # TypeBuildTag="[NON-CFW][PRIVATE-LTO-SLMK]"
 # CompileClangLTOKernel && CleanOut
