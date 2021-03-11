2996734d9b2a10356e7380eb88331d25630c172d
ZyCromerZ <neetroid97@gmail.com>
2021-03-11 10:42:51 +0700

    compile full lto and gcc only for test plus saos kernel dah fixed maybe
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/master.sh
+++ for/master.sh
@@ -1,21 +1,30 @@
 #! /bin/bash
-KernelBranch="20210205/neutrino-3-gcc"
+KernelBranch="20210205/neutrino-3-custom"
 
 IncludeFiles "${MainPath}/device/begonia-q-oss.sh"
 CustomUploader="Y"
 IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
 FolderUp="keqing-drive"
-# TypeBuildTag="[NON-CFW][PRIVATE][SLMK]"
-TypeBuildTag="[NON-CFW][Stock-LMK]"
+TypeBuildTag="[NON-CFW][PRIVATE][SLMK][FULL-LTO]"
+# TypeBuildTag="[NON-CFW][Stock-LMK]"
 
 CloneKernel "--depth=1"
 # CloneGccten
 # CloneCompiledGcc
 CloneCompiledGccEleven
 # CompileGccKernel && CleanOut
-# CloneGugelClang
-# CompileClangKernel && CleanOut  
+CloneGugelClang
+CompileClangKernel && CleanOut
 # pullSlmk && PullLtoSlmk
 # TypeBuildTag="[NON-CFW][PRIVATE-LTO-SLMK]"
 # CompileClangLTOKernel && CleanOut
-CompileGccKernel
+# CompileGccKernel
+
+KernelBranch="20210205/neutrino-3-gcc"
+CloneKernel "--depth=1"
+TypeBuildTag="[NON-CFW][Stock-LMK]"
+CompileGccKernel && CleanOut
+
+# cleanup stuff after done
+cd "${MainPath}"
+rm -rf *
