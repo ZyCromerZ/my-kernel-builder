e25c297d57c8c29e52041ff4cf3a15d2d4e525d8
ZyCromerZ <neetroid97@gmail.com>
2021-03-11 07:24:29 +0700

    test gcc 11 + change branch
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/master.sh
+++ for/master.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
-KernelBranch="20210205/neutrino-3-custom"
+KernelBranch="20210205/neutrino-3-gcc"
 
 IncludeFiles "${MainPath}/device/begonia-q-oss.sh"
 CustomUploader="Y"
 IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
 FolderUp="keqing-drive"
-TypeBuildTag="[NON-CFW][PRIVATE][SLMK]"
+# TypeBuildTag="[NON-CFW][PRIVATE][SLMK]"
+TypeBuildTag="[NON-CFW][Stock-LMK]"
 
-CloneKernel
+CloneKernel "--depth=1"
 # CloneGccten
-CloneCompiledGcc
+# CloneCompiledGcc
+CloneCompiledGccEleven
 # CompileGccKernel && CleanOut
-CloneGugelClang
-CompileClangKernel && CleanOut  
+# CloneGugelClang
+# CompileClangKernel && CleanOut  
 # pullSlmk && PullLtoSlmk
 # TypeBuildTag="[NON-CFW][PRIVATE-LTO-SLMK]"
 # CompileClangLTOKernel && CleanOut
-
-# CloneOldDTCClang
-# pullSlmk && PullLtoSlmk
-# TypeBuildTag="[NON-CFW][PRIVATE-LTO-SLMK]"
-# CompileClangLTOKernel && CleanOut
+CompileGccKernel
