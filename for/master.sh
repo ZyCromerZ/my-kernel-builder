4fef264b42d4a3091584b4764a1d1c7463e7f3d4
ZyCromerZ <neetroid97@gmail.com>
2021-03-11 18:43:21 +0700

    test xobod
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/master.sh
+++ for/master.sh
@@ -1,29 +1,39 @@
 #! /bin/bash
-KernelBranch="20210205/neutrino-3-custom"
+# KernelBranch="20210205/neutrino-3-custom"
+KernelBranch="lineage-18.1-R-wifitags"
 
-IncludeFiles "${MainPath}/device/begonia-q-oss.sh"
+# IncludeFiles "${MainPath}/device/begonia-q-oss.sh"
+IncludeFiles "${MainPath}/device/x01bd.sh"
 CustomUploader="Y"
-IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
+# IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/x01bd_kernel"
+
 FolderUp="keqing-drive"
-TypeBuildTag="[NON-CFW][PRIVATE][SLMK][FULL-LTO]"
+# TypeBuildTag="[NON-CFW][PRIVATE][SLMK][FULL-LTO]"
 # TypeBuildTag="[NON-CFW][Stock-LMK]"
 
 CloneKernel "--depth=1"
-# CloneGccten
-# CloneCompiledGcc
-CloneCompiledGccEleven
-# CompileGccKernel && CleanOut
-CloneGugelClang
-CompileClangKernel && CleanOut
+# # CloneGccten
+# # CloneCompiledGcc
+# CloneCompiledGccEleven
+# # CompileGccKernel && CleanOut
+# CloneGugelClang
+# CompileClangKernel && CleanOut
 # pullSlmk && PullLtoSlmk
 # TypeBuildTag="[NON-CFW][PRIVATE-LTO-SLMK]"
 # CompileClangLTOKernel && CleanOut
 # CompileGccKernel
 
-KernelBranch="20210205/neutrino-3-gcc"
-CloneKernel "--depth=1"
-TypeBuildTag="[NON-CFW][Stock-LMK]"
-CompileGccKernel && CleanOut
+# KernelBranch="20210205/neutrino-3-gcc"
+# CloneKernel "--depth=1"
+# TypeBuildTag="[NON-CFW][Stock-LMK]"
+# CompileGccKernel && CleanOut
+
+# ------ --------
+CloneGugelClang
+CloneGCCOld
+CompileClangKernel && CleanOut
+TypeBuildTag="[R][TEST-WIFI]"
 
 # cleanup stuff after done
 cd "${MainPath}"
