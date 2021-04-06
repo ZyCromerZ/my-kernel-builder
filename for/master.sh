fdef2d45b656317a3c6e26ad7a446de27a646b05
ZyCromerZ <neetroid97@gmail.com>
2021-04-06 09:14:03 +0700

    test new uploader
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/master.sh
+++ for/master.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 # KernelBranch="20210405/main"
-
+MainPath="$(pwd)"
+echo "test upload" > file.txt
+git clone https://${GIT_SECRET}@github.com/ZyCromerZ/uploader-kernel-private $MainPath/up
+cd $MainPath/up
+chmod +x run.sh
+. run.sh $MainPath/file.txt "keqing-drive" "TEST Upload"
 # IncludeFiles "${MainPath}/device/lancelot-q-oss.sh"
 # CustomUploader="Y"
 # IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/lancelot_kernels"
