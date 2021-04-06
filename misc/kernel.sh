3c75cf5b8d9750f228958c5084754e46258bccc4
ZyCromerZ <neetroid97@gmail.com>
2021-04-06 09:30:04 +0700

    misc: kernel: update uploader path links
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- misc/kernel.sh
+++ misc/kernel.sh
@@ -16,7 +16,7 @@ getInfoErr() {
 if [ ! -z "$1" ];then
     KernelRepo="$1"
     if [ "$CustomUploader" == "Y" ];then
-        git clone https://$GIT_SECRET@github.com/$GIT_USERNAME/gdrive_uploader -b master "${UploaderPath}"  --depth=1 
+        git clone https://${GIT_SECRET}@github.com/${GIT_USERNAME}/uploader-kernel-private -b master "${UploaderPath}"  --depth=1
     fi
     if [ "$UseSpectrum" == "Y" ];then
         git clone https://github.com/ZyCromerZ/Spectrum -b master "${SpectrumPath}"  --depth=1 
