648eb2327177b9aea18ae9b536f64167b6206452
ZyCromerZ <neetroid97@gmail.com>
2021-04-07 22:01:21 +0700

    test
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/master.sh
+++ for/master.sh
@@ -1,22 +1,16 @@
 #! /bin/bash
-# KernelBranch="20210405/main"
-MainPath="$(pwd)"
-echo "test upload" > file.txt
-git clone https://${GIT_SECRET}@github.com/ZyCromerZ/uploader-kernel-private $MainPath/up
-cd $MainPath/up
-chmod +x run.sh
-. run.sh $MainPath/file.txt "keqing-drive" "TEST Upload"
-# IncludeFiles "${MainPath}/device/lancelot-q-oss.sh"
-# CustomUploader="Y"
-# IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/lancelot_kernels"
-# FolderUp="keqing-drive"
-# TypeBuildTag="[STOCK]"
+KernelBranch="eleven-upstream-mod2"
+IncludeFiles "${MainPath}/device/lancelot-q-oss.sh"
+CustomUploader="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/lancelot_kernels"
+FolderUp="keqing-drive"
+TypeBuildTag="[TEST]"
 
-# CloneKernel "--depth=1"
-# CloneCompiledGccEleven
-# CloneGugelClang
-# CompileClangKernel
+CloneKernel "--depth=1"
+CloneGccten
+CloneGugelClang
+CompileClangKernel
 
-# # cleanup stuff after done
-# cd "${MainPath}"
-# rm -rf *
+# cleanup stuff after done
+cd "${MainPath}"
+rm -rf *
