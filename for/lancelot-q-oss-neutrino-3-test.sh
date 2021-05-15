bd7fa94570a496b38900877d321df9a680b3044d
ZyCromerZ <neetroid97@gmail.com>
2021-05-15 13:38:56 +0700

    test :)
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/lancelot-q-oss-neutrino-3-test.sh
+++ for/lancelot-q-oss-neutrino-3-test.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-KernelBranch="eleven-upstream-mod2"
+KernelBranch="20210405/neutrino-3"
 
 IncludeFiles "${MainPath}/device/lancelot-q-oss.sh"
 CustomUploader="Y"
 IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/lancelot_kernels"
 FolderUp="keqing-drive"
-TypeBuildTag="[TEST][D]"
+TypeBuildTag="[TEST][B]"
 
 CloneKernel
-CloneCompiledGccTwelve
-#  && PullLtoALmk
-CompileGccKernel && CleanOut
+CloneCompiledGccTwelve 
+CloneOldDTCClang && PullLtoSlmk
+CompileClangKernel && CleanOut
 
 
 # cleanup stuff after done
