e10f5875f0fe6cd87265a80407edc4b021be58ee
ZyCromerZ <neetroid97@gmail.com>
2021-05-17 12:58:37 +0700

    add vayu builder
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ device/vayu-r-oss.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+DEVICE="Poco X3 pro"
+CODENAME="Vayu"
+SaveChatID="-1001301538740"
+ARCH="arm64"
+TypeBuild="Stable"
+DEFFCONFIG="vayu_defconfig"
+GetBD=$(date +"%m%d")
+GetCBD=$(date +"%Y-%m-%d")
+TypeBuildTag="[R-OSS]"
+FolderUp="keqing-drive"
+ExFolder=""
+AnyKernelBranch="master-vayu"
+FirstSendInfoLink="N"
