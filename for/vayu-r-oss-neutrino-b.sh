e623a09f8e79e453a1f85c79c5bdfb2e3c1e2268
ZyCromerZ <neetroid97@gmail.com>
2021-05-19 03:44:51 +0700

    slmk vayu
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/vayu-r-oss-neutrino-b.sh
+++ for/vayu-r-oss-neutrino-b.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-KernelBranch="20210517/neutrino-b"
+KernelBranch="20210517/neutrino-b-SLMK"
 
 IncludeFiles "${MainPath}/device/vayu-r-oss.sh"
 CustomUploader="Y"
 IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/vayu_kernel"
 FolderUp="keqing-drive"
-TypeBuildTag="[ThinLTO][R-OSS]"
+TypeBuildTag="[SLMK][ThinLTO][R-OSS]"
 
 # misc
 # doOsdnUp=$FolderUp
