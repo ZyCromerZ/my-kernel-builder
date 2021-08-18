f8cdfe505f07b6bf9ec5cf45300005551ca89b3c
ZyCromerZ <neetroid97@gmail.com>
2021-08-18 22:00:05 +0700

    misc: gcc: update link
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- misc/gcc.sh
+++ misc/gcc.sh
@@ -29,16 +29,16 @@ CloneCompiledGccEleven(){
     rm -rf ${GCCaPath}/aarch64-zyc-linux-gnu ${GCCbPath}/arm-zyc-linux-gnueabi
     mkdir "${GCCaPath}"
     mkdir "${GCCbPath}"
-    if [ ! -e "${MainPath}/arm-zyc-linux-gnueabi-11.x-gnu-20210808.tar.gz" ];then
-        wget -q  https://github.com/ZyCromerZ/compiled-gcc/releases/download/varm-zyc-linux-gnueabi-11.x-gnu-20210808/arm-zyc-linux-gnueabi-11.x-gnu-20210808.tar.gz
+    if [ ! -e "${MainPath}/arm-zyc-linux-gnueabi-11.x-gnu-20210818.tar.gz" ];then
+        wget -q  https://github.com/ZyCromerZ/compiled-gcc/releases/download/varm-zyc-linux-gnueabi-11.x-gnu-20210818/arm-zyc-linux-gnueabi-11.x-gnu-20210818.tar.gz
     fi
-    tar -xf arm-zyc-linux-gnueabi-11.x-gnu-20210808.tar.gz -C $GCCbPath
+    tar -xf arm-zyc-linux-gnueabi-11.x-gnu-20210818.tar.gz -C $GCCbPath
     GCCbPath="${GCCbPath}/arm-zyc-linux-gnueabi"
     for32=arm-zyc-linux-gnueabi
-    if [ ! -e "${MainPath}/aarch64-zyc-linux-gnu-11.x-gnu-20210808.tar.gz" ];then
-        wget -q https://github.com/ZyCromerZ/compiled-gcc/releases/download/vaarch64-zyc-linux-gnu-11.x-gnu-20210808/aarch64-zyc-linux-gnu-11.x-gnu-20210808.tar.gz
+    if [ ! -e "${MainPath}/aarch64-zyc-linux-gnu-11.x-gnu-20210818.tar.gz" ];then
+        wget -q https://github.com/ZyCromerZ/compiled-gcc/releases/download/vaarch64-zyc-linux-gnu-11.x-gnu-20210818/aarch64-zyc-linux-gnu-11.x-gnu-20210818.tar.gz
     fi
-    tar -xf aarch64-zyc-linux-gnu-11.x-gnu-20210808.tar.gz -C $GCCaPath
+    tar -xf aarch64-zyc-linux-gnu-11.x-gnu-20210818.tar.gz -C $GCCaPath
     GCCaPath="${GCCaPath}/aarch64-zyc-linux-gnu"
     for64=aarch64-zyc-linux-gnu
     GetGccVersion
