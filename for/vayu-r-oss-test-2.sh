2702de440b938c7c73c14d6deb1a70ee7de42d22
ZyCromerZ <neetroid97@gmail.com>
2021-08-25 02:56:17 +0700

    vayu: another type kernel
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ for/vayu-r-oss-test-2.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+KernelBranch="20210824/neutrino-hareudang"
+
+IncludeFiles "${MainPath}/device/vayu-r-oss.sh"
+CustomUploader="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/vayu_kernel"
+# FolderUp="shared-file"
+TypeBuildTag="[Stable]"
+
+# misc
+# doOsdnUp=$FolderUp
+# doSFUp=$FolderUp
+ 
+
+CloneKernel "--depth=1"
+# CloneGCCOld && CloneGugelClang
+# CompileClangKernel && CleanOut
+CloneProtonClang
+CompileProtonClangKernel && CleanOut
+# CloneCompiledGccTwelve
+CloneCompiledGccEleven
+# CloneSdClang
+# CompileClangKernel && CleanOut
+CloneDTCClang
+CompileClangKernel && CleanOut
+DEFFCONFIG="vayu_gcc_defconfig"
+CompileGccKernel
