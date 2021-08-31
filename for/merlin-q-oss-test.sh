334ade188df87f9eadac918eb01d1ab1a1c3ef04
ZyCromerZ <neetroid97@gmail.com>
2021-08-31 11:04:59 +0700

    merlin: add test kernel
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ for/merlin-q-oss-test.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+KernelBranch="20210405/qk-l"
+
+IncludeFiles "${MainPath}/device/merlin-q-oss.sh"
+CustomUploader="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/lancelot_kernels"
+FolderUp="shared-file"
+# doSFUp=$FolderUp
+# doOsdnUp=$FolderUp
+TypeBuildTag="[STABLE]"
+
+CloneKernel
+CloneCompiledGccEleven
+CloneElevenGugelClang
+# CompileGccKernel && pullALmk
+pullSlmk
+CompileClangKernel && CleanOut
+CompileGccKernel
+# CompileGccKernel && CleanOut
+
+
+# cleanup stuff after done
+cd "${MainPath}"
+rm -rf *
